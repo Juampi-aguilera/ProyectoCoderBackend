d498a64ceea48ec2cbffc7dbb879292ad7df0cd5
juan aguilera <juampiag3@gmail.com>
2023-05-03 23:09:54 -0300

    agregando sessions

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ import handlebars from "express-handlebars"
 import { Server } from "socket.io";
 import mongoose from 'mongoose'
 import productsModel from './models/products.model.js'
+import MongoStore from 'connect-mongo';
+import session from 'express-session';
+import usersRouter from "./routes/users.routes.js"
+import sessionsRouter from './routes/sessions.routes.js'
 
 let manager = new ProductManager("./files/products.json")
 const app = express()
@@ -20,9 +24,11 @@ const httpServer = app.listen(PORT, ()=>{
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+const mongoURL="mongodb+srv://admin:1234@cluster0.ttuc4ws.mongodb.net/products"
+
 const connectMongoDB= async()=>{
     try{
-        await mongoose.connect("mongodb+srv://admin:1234@cluster0.ttuc4ws.mongodb.net/products")
+        await mongoose.connect(mongoURL)
         console.log("Conectado con exito a MongoDB usando Moongose.");
         // let response=await productsModel.insertMany([{"title":"Kill Bill","description":"Cuadro de Kill Bill","category":"Cine y Series","price":6000,"thumbnail":["https://postercity.com.ar/wp-content/uploads/2019/09/kill-bill-510x765.jpg"],"code":1,"status":true,"stock":true},{"title":"Pulp Fiction","description":"Cuadro de Pulp Fiction","category":"Cine y Series","price":6000,"thumbnail":["https://postercity.com.ar/wp-content/uploads/2017/07/Pulpfiction-510x744.jpg"],"code":2,"status":true,"stock":true},{"title":"john lennon pop","description":"Cuadro de john lennon pop","category":"Musica","price":4000,"thumbnail":["https://postercity.com.ar/wp-content/uploads/2022/07/john-lennon-pop-510x680.jpg"],"code":3,"status":true,"stock":true}])
         // console.log(response)
@@ -36,6 +42,17 @@ const connectMongoDB= async()=>{
 }
 connectMongoDB()
 
+app.use(session({
+    store:MongoStore.create({
+        mongoUrl:mongoURL,
+        mongoOptions: {useNewUrlParser: true, useUnifiedTopology: true},
+        ttl: 40
+    }),
+    secret:"encriptacion",
+    resave: false,
+    saveUninitialized: true
+}))
+
 // configuracion de hbs
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname+'/views')
@@ -44,6 +61,8 @@ app.set('view engine', 'handlebars')
 // carpeta public
 app.use(express.static( __dirname + "/public"))
 
+app.use('/users',usersRouter);
+app.use('/api/sessions',sessionsRouter);
 app.use("/api/products", productsRouter)
 app.use("/api/carts", cartsRouter)
 app.use("/api/realtimeproducts", realtimeproductsRouter)
